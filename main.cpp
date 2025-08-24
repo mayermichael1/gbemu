b457af1f5803c40eb142581cc8233d445fd0c391
mayermichael1 <michael.mayer2105@gmail.com>
2025-08-24 18:03:04 +0200

    implement simple shader programs

--- main.cpp
+++ main.cpp
@@ -9,6 +9,92 @@
 
 #include <GLFW/glfw3.h>
 
+
+void
+compile_and_attach_shader(u32 program, GLenum shader_type, const char *source)
+{
+    u32 shader = 0;
+    shader = glCreateShader(shader_type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+    glAttachShader(program, shader);
+    glDeleteShader(shader);
+
+    GLint success = GL_FALSE;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        GLint info_length = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_length);
+        u8 *info = (u8*)malloc(info_length);
+        glGetShaderInfoLog(
+            shader,
+            info_length,
+            NULL,
+            (GLchar*)info
+        );
+        //TODO: printf and free should be removed
+        printf("Shader Compilation Error:\t\n%s\n",info);
+        free(info);
+    }
+}
+
+u32 
+create_program()
+{
+    u32 program = glCreateProgram();
+
+    compile_and_attach_shader(program, GL_VERTEX_SHADER, R"STRING(
+#version 450 core
+
+void
+main(void)
+{
+    vec4[4] vertices = vec4[](
+        vec4(-0.5, -0.5, 0.0, 1.0),
+        vec4( 0.5, -0.5, 0.0, 1.0),
+        vec4( 0.5,  0.5, 0.0, 1.0),
+        vec4(-0.5,  0.5, 0.0, 1.0)
+    );
+    gl_Position = vertices[gl_VertexID];
+}
+    )STRING");
+
+compile_and_attach_shader(program, GL_FRAGMENT_SHADER, R"STRING(
+#version 450 core
+
+out vec4 color;
+
+void
+main(void)
+{
+    color = vec4(1.0);
+}
+    )STRING");
+
+    glLinkProgram(program);
+
+    GLint success = GL_FALSE;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        GLint info_length = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &info_length);
+        u8 *info = (u8*)malloc(info_length);
+        glGetProgramInfoLog(
+            program,
+            info_length,
+            NULL,
+            (GLchar*)info
+        );
+        printf("Shader Linking Error:\t\n%s\n",info);
+        free(info);
+        program = 0;
+    }
+
+    return(program);
+}
+
 s32 
 main (void)
 {
@@ -33,6 +119,9 @@ main (void)
 
     if(window)
     {
+        u32 program = create_program();
+        glUseProgram(program);
+
         while(running)
         {
             glfwPollEvents();
@@ -42,6 +131,7 @@ main (void)
                 running = false;
             }
             glClear(GL_COLOR_BUFFER_BIT);
+            glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
             glfwSwapBuffers(window);
         }
     }
